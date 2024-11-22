24c70a506d786222b04df65376823ddd11a91407
JEunseo <jjeseo1010@gmail.com>
2024-11-22 16:38:21 +0900

    Build : remoteMonster 삭제, main 클래스 지정

---
+++ app/src/main/java/org/example/gymbuddyback/config/WebRtcConfig.java
@@ -0,0 +1,12 @@
+package java.org.example.gymbuddyback.config;
+
+
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+public class WebRtcConfig implements WebSocketConfigurer {
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+
+    }
+}
