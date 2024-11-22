a274e39e3d89c900e0e39612440d52e296455434
JEunseo <jjeseo1010@gmail.com>
2024-11-22 17:20:02 +0900

    Test : WebRtc config 테스트

--- app/src/main/java/org/example/gymbuddyback/config/WebRtcConfig.java
+++ app/src/main/java/org/example/gymbuddyback/config/WebRtcConfig.java
@@ -1,12 +1,17 @@
 package java.org.example.gymbuddyback.config;
 
 
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import sun.misc.SignalHandler;
 
+@EnableWebSocket
 public class WebRtcConfig implements WebSocketConfigurer {
-    @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+    private final SignalHandler signalHandler;
 
+    public WebRtcConfig(SignalHandler signalHandler) {
+        this.signalHandler = signalHandler;
     }
+
 }
