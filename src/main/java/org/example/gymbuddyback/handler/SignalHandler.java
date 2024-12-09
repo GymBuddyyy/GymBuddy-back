4f123cc8616070b634d2336ae8f0b4d3bc8e8544
JEunseo <jjeseo1010@gmail.com>
2024-12-09 15:51:32 +0900

    Feat : WebSocket 핸들러 추가

---
+++ src/main/java/org/example/gymbuddyback/handler/SignalHandler.java
@@ -0,0 +1,8 @@
+package org.example.gymbuddyback.handler;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@Component
+public class SignalHandler extends TextWebSocketHandler {
+}
